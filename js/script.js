38982dd6f937b503d34869da256daf5d4f0522b3
geekclick <anujghom27@gmail.com>
2024-07-16 00:00:04 +0530

    responsive design

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
 
 $(document).ready(function () {
-    $('.project img').hide(); // Hide all images initially
-    $('#projectImage2').show(); // Show the second image by default
+    $('.project img').hide();
+    $('#projectImage2').show();
 
     $('.project-cards .card').click(function () {
         var imageIndex = $(this).data('image');
-        console.log(imageIndex)
-        $('.project img').hide(); // Hide all images
-        $('#projectImage' + imageIndex).show(); // Show the selected image
+        $('.project img').hide();
+        $('#projectImage' + imageIndex).show();
 
-        // Optional: Highlight the selected card
         $('.project-cards .card').removeClass('active');
         $(this).addClass('active');
     });
@@ -19,49 +17,35 @@ $(document).ready(function () {
     var slides = $('.slide');
     var dotsContainer = $('.slider-dots');
 
-    // Create dots
     slides.each(function (index) {
         index % 2 == 0 && dotsContainer.append('<span class="dot" data-index="' + index + '"><span></span></span>');
     });
 
 
     var dots = $('.dot');
-    dots.eq(0).addClass('active'); // Set the initial active dot
+    dots.eq(0).addClass('active');
 
     function updateDots() {
         var scrollLeft = slider.scrollLeft();
         var slideWidth = slides.first().outerWidth(true);
-        console.log(scrollLeft)
-        console.log(slideWidth)
         var currentIndex = Math.round(((scrollLeft) / slideWidth));
-        console.log((scrollLeft) / slideWidth);
         dots.removeClass('active');
         dots.eq(currentIndex).addClass('active');
     }
 
-    // Set up scroll event
     slider.on('scroll', function () {
         updateDots();
     });
 
-    // Dot click event
     dots.click(function () {
         var index = $(this).data('index');
         var slideWidth = slides.first().outerWidth(true);
         slider.animate({ scrollLeft: slideWidth * index }, 500);
     });
 
-    // Initial update of dots
     updateDots();
 
-    // Optional: Automatic slide change
-    // setInterval(function() {
-    //     var nextIndex = (dots.filter('.active').data('index') + 1) % slides.length;
-    //     dots.eq(nextIndex).click();
-    // }, 5000); // Change slide every 5 seconds
-
     $('#contactForm').on('click', function (event) {
-        // Simulate form submission (you may replace this part with your actual form submission logic)
         $.ajax({
             url: this.action,
             method: this.method,
@@ -69,9 +53,7 @@ $(document).ready(function () {
             processData: false,
             contentType: false,
             success: function (response) {
-                // Reset the form
                 $('#contactForm')[0].reset();
-                // Disable the submit button
                 $('.submit-btn').prop('disabled', true);
             },
             error: function () {
